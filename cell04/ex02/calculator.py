98757262e2c2c1d9dbd282fdb712b48bcc629525
tinboonwan <tinboonwan2006@gmail.com>
2025-06-17 10:03:45 +0700

    Create calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+if num2 != 0:
+   division = num1 / num2
+else:
+   division = "undefined (cannot divide by zero)"
+print(f"{num1} + {num2} = {addition}")
+print(f"{num1} - {num2} = {subtraction}")
+print(f"{num1} * {num2} = {multiplication}")
+print(f"{num1} / {num2} = {division}")
