77fb27569da0a2e98abf85ea8170097da6c4e738
tinboonwan <tinboonwan2006@gmail.com>
2025-06-17 09:45:36 +0700

    Create i_got_that.py

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,6 @@
+while True:
+    user_input = input("Say something (type 'STOP' to exit): ")
+    if user_input.upper() == "STOP":
+    print("Stopping the program.")
+    break
+    print(f"I got that: {user_input}")
